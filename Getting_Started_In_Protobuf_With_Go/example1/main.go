ec8f619d87508f46291bf139e89bb401e4431f87
Shubham Srivastava <shubhamsrivastava@Srivastava-Shubham.local>
2021-02-06 20:26:12 +0530

    Getting_Started_In_Protobuf_With_Go: Update

--- Getting_Started_In_Protobuf_With_Go/example1/main.go
+++ Getting_Started_In_Protobuf_With_Go/example1/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"example1/pb"
 	"fmt"
 )
 
 func main() {
-	var person Person
+	var person pb.Person
 
 	person.Id = 1001
 	person.Email = "abc@xyz.in"
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/sri-shubham/blogcode
-
-go 1.15
-
-require (
-	github.com/golang/protobuf v1.4.1
-	google.golang.org/protobuf v1.25.0
-)
